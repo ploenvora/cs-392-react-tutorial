fa99081043da08596d7ecba11d57c0a95ef63bce
ploenvora <p.voraprukpisut@gmail.com>
2023-09-22 17:22:24 -0500

    Term filter

--- src/App.jsx
+++ src/App.jsx
@@ -3,47 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
-import { useJsonQuery } from './utilities/fetch';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
-
 const queryClient = new QueryClient()
+const title = "CS Courses for 2018-2019"
 
 const App = () => {
   return (
     <Fragment>
       <QueryClientProvider client={queryClient}>
-        <Banner title={schedule.title}></Banner>
-        <CourseList courses={schedule.courses}></CourseList>
+        <Banner title={title}></Banner>
+        <CourseList></CourseList>
       </QueryClientProvider>
     </Fragment>
   )
--- src/components/FilterTerm.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const FilterButton = () => (
-    <div>
-      <input/>
-      <label>Filter term</label>
-    </div>
-);
