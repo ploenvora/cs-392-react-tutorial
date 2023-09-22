1013213f09ea83f0c9692e87030384d25bdcf84b
ploenvora <p.voraprukpisut@gmail.com>
2023-09-22 15:58:31 -0500

    Fetch data

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -4,26 +4,29 @@ import { useJsonQuery } from "../utilities/fetch";
 const CourseList = (props) => {
     const url = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
     const [ data, isLoading, error ] = useJsonQuery(url)
+
     return (
-        <div className={styles.courses}>
-            {Object.entries(data.courses).map(([courseCode, courseDetails]) => (
-                <div key={courseCode} className={styles.course}>
-                    <div className={styles.info}>
-                        <h2>
-                            {courseDetails.term} CS {courseCode}
-                        </h2>
-                        <p>
-                            {courseDetails.title}
-                        </p>
+        data ? (
+            <div className={styles.courses}>
+                {Object.entries(data.courses).map(([courseCode, courseDetails]) => (
+                    <div key={courseCode} className={styles.course}>
+                        <div className={styles.info}>
+                            <h2>
+                                {courseDetails.term} CS {courseCode}
+                            </h2>
+                            <p>
+                                {courseDetails.title}
+                            </p>
+                        </div>
+                        <div className={styles.meets}>
+                            <p>
+                                {courseDetails.meets}
+                            </p>
+                        </div>
                     </div>
-                    <div className={styles.meets}>
-                        <p>
-                            {courseDetails.meets}
-                        </p>
-                    </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        ) : <p>Loading...</p>
     )
 };
 
